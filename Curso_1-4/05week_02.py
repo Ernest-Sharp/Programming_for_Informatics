5b26a023e3e83823abeabfe62384ad211267cee9
Ernest-Sharp <es29192@email.vccs.eu>
2016-05-14 17:18:29 -0500

    Ejercicios semana 5 y 6 del curso

---
+++ Curso_1-4/05week_02.py
@@ -0,0 +1,39 @@
+#3.3 Write a program to prompt for a score between 0.0
+# and 1.0. If the score is out of range, print an error.
+# If the score is between 0.0 and 1.0, print a grade
+# using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a
+# suitable error message and exit. For the test,
+# enter a score of 0.85.
+
+a = raw_input("Enter a score between 0.0 and 1.0: ")
+
+try:
+    score = float(a)
+except:
+    print "Please, enter a number between 0.0 and 1.0"
+    quit()
+
+if score > 1:
+    print "Please, enter a number between 0.0 and 1.0"
+    quit()
+elif score < 0:
+    print "Please, enter a number between 0.0 and 1.0"
+    quit()
+elif score >= 0.9:
+    grade = "A"
+elif score >= 0.8:
+    grade = "B"
+elif score >= 0.7:
+    grade = "C"
+elif score >= 0.6:
+    grade = "D"
+elif score < 0.6:
+    grade = "F"
+print grade
